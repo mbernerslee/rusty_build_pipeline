efe7ce0b8e37717711fdd47a878b8cc89c4dfa41
berners <matthewbernerslee@gmail.com>
2023-12-19 10:37:56 +0000

    updated the flake to have a non archaic version of rust

--- flake.nix
+++ flake.nix
@@ -1,22 +1,41 @@
+#{
+#  inputs = {
+#    naersk.url = "github:nix-community/naersk/master";
+#    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+#    utils.url = "github:numtide/flake-utils";
+#  };
+#
+#  outputs = { self, nixpkgs, utils, naersk }:
+#    utils.lib.eachDefaultSystem (system:
+#      let
+#        pkgs = import nixpkgs { inherit system; };
+#        naersk-lib = pkgs.callPackage naersk { };
+#      in
+#      {
+#        defaultPackage = naersk-lib.buildPackage ./.;
+#        devShell = with pkgs; mkShell {
+#          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
+#          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+#          LOCALE_ARCHIVE=/usr/lib/locale/locale-archive;
+#        };
+#      });
+#}
 {
   inputs = {
-    naersk.url = "github:nix-community/naersk/master";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    utils.url = "github:numtide/flake-utils";
-  };
-
-  outputs = { self, nixpkgs, utils, naersk }:
-    utils.lib.eachDefaultSystem (system:
+      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+      flake-utils.url = "github:numtide/flake-utils";
+      rust-overlay.url = "github:oxalica/rust-overlay";
+    };
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
-        naersk-lib = pkgs.callPackage naersk { };
-      in
-      {
-        defaultPackage = naersk-lib.buildPackage ./.;
-        devShell = with pkgs; mkShell {
-          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
-          LOCALE_ARCHIVE=/usr/lib/locale/locale-archive;
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rustVersion = pkgs.rust-bin.stable.latest.default;
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs =
+            [ (rustVersion.override { extensions = [ "rust-src" ]; }) ];
         };
       });
 }

c58c28b0d306bbf40e10e8f77a980652c3f4554b
berners <matthewbernerslee@gmail.com>
2023-12-26 11:15:28 +0000

    can create config file with init

--- src/init.rs
+++ src/init.rs
@@ -1,4 +1,3 @@
-//use super::exit;
 use config::Config;
 
 mod config;
--- src/init/directory_structure.rs
+++ src/init/directory_structure.rs
@@ -2,37 +2,41 @@ use super::config::Config;
 
 use std::fs;
 use std::fs::File;
+use std::io::Write;
 use std::path::PathBuf;
 
 // https://stackoverflow.com/questions/49286608/what-is-the-proper-way-to-pass-a-module-as-an-argument
 pub fn create(config: Config) -> Result<(), String> {
     let working_directory_path = working_directory_path(&config.working_directory);
     let build_pipeline_path = build_pipeline_path(&working_directory_path);
+    let config_file_path = config_file_path(&build_pipeline_path);
 
-    create_working_directory(&working_directory_path)?;
-    create_build_pipeline_directory(&build_pipeline_path)
+    check_working_directory_exists(&working_directory_path)?;
+    create_build_pipeline_directory(&build_pipeline_path)?;
+    create_config_file(&config_file_path)
 }
 
-fn create_working_directory(working_directory_path: &PathBuf) -> Result<(), String> {
+fn check_working_directory_exists(working_directory_path: &PathBuf) -> Result<(), String> {
     if working_directory_path.exists() {
-        Err(String::from("I failed because the build_pipeline directory I wanted to create already exists. Maybe you've already initialised before?"))
+        Ok(())
     } else {
-        match fs::create_dir_all(working_directory_path) {
-            Ok(_) => Ok(()),
-            _ => Err(String::from("I failed because the directory I wanted to create already exists. Maybe you've already initialised before?")),
-        }
+        Err(String::from("I failed because the directory you supplied does not exist. I can only initialise in a directory that already exists."))
     }
 }
 
 fn create_build_pipeline_directory(build_pipeline_path: &PathBuf) -> Result<(), String> {
     if build_pipeline_path.exists() {
-        Err(String::from("I failed because the build_pipeline directory I wanted to create already exists. Maybe you've already initialised before?"))
+        Err(create_build_pipeline_directory_error())
     } else {
-        match File::create(build_pipeline_path) {
+        match fs::create_dir(build_pipeline_path) {
             Ok(_) => Ok(()),
-            _ => Err(String::from("I failed because the build_pipeline directory I wanted to create already exists. Maybe you've already initialised before?")),
+            _ => Err(create_build_pipeline_directory_error()),
         }
     }
+}
+
+fn create_build_pipeline_directory_error() -> String {
+    String::from("I failed because the build_pipeline directory I wanted to create already exists. Maybe you've already initialised before?")
 }
 
 fn working_directory_path(working_dir_path: &String) -> PathBuf {
@@ -47,11 +51,65 @@ fn build_pipeline_path(working_directory_path: &PathBuf) -> PathBuf {
     path
 }
 
-//fn config_file_path(build_pipeline_path: &PathBuf) -> PathBuf {
-//    let mut path = build_pipeline_path.clone();
-//    path.push(CONFIG_JSON);
-//    path
-//}
+fn config_file_path(build_pipeline_path: &PathBuf) -> PathBuf {
+    let mut path = build_pipeline_path.clone();
+    path.push(CONFIG_JSON);
+    path
+}
+
+fn create_config_file(config_file_path: &PathBuf) -> Result<(), String> {
+    match create_empty_config_file(config_file_path) {
+        Ok(config_file) => write_config_file(config_file),
+        Err(error) => Err(error),
+    }
+}
+
+fn write_config_file(mut config_file: File) -> Result<(), String> {
+    match config_file.write_all(CONFIG) {
+        Ok(()) => Ok(()),
+        _ => Err(String::from("I failed because I could not write to the build_pipeline/config.json file, which I have created but have left empty.")),
+    }
+}
+
+fn create_empty_config_file(config_file_path: &PathBuf) -> Result<File, String> {
+    match File::create(config_file_path) {
+        Ok(config_file) => Ok(config_file),
+        _ => Err(String::from(
+            "I failed because I could not create the build_pipeline/config.json file",
+        )),
+    }
+}
 
 const BUILD_PIPELINE: &'static str = "build_pipeline";
-//const CONFIG_JSON: &'static str = "config.json";
+const CONFIG_JSON: &'static str = "config.json";
+const CONFIG: &'static [u8] = r#"[
+  {
+    "buildStepName": "check tires",
+    "commandType": "shellCommand",
+    "command": "echo tires OK!",
+    "dependsOn": []
+  },
+  {
+    "buildStepName": "check enough fuel",
+    "commandType": "shellCommand",
+    "command": "echo fuel OK!",
+    "dependsOn": [],
+    "envVars": [
+      {
+        "name": "SOME_ENV_VAR",
+        "value": "A_COOL_ENV_VAR"
+      }
+    ]
+  },
+  {
+    "buildStepName": "drive car",
+    "commandType": "shellCommand",
+    "command": "echo I am driving OK!",
+    "dependsOn": [
+      "check tires", "check enough fuel"
+    ]
+  }
+]
+
+"#
+.as_bytes();
